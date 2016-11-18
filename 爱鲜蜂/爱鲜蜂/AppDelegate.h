a68428d0aa91dabf18328f50ae72f65bcb4626e7
YmiroS <121317202@qq.com>
2016-11-18 10:14:24 +0800

    1.添加新项目 2.集成第三方框架 3.图片素材 4.整理文件夹格式 5.pch文件 可以进行小组开发...根据需求添加剩余图片素材

---
+++ 爱鲜蜂/爱鲜蜂/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  爱鲜蜂
+//
+//  Created by 杨烁 on 2016/11/18.
+//  Copyright © 2016年 杨烁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
